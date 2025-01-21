231ebcbccf9e6a73640da957bae3518c23e6b791
Subhadip Bera <subhodipbera10@gmail.com>
2025-01-21 23:18:44 +0530

    add animetion

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 var typed = new Typed('.text', {
-    strings: ['Web Developer', 'Jva with DSA','Forntend Developer'],
+    strings: ['Web Developer', 'Jva with DSA','Forntend Developer','Ex Intern of D.S.A','Student of TNU'],
     typeSpeed: 50,
+    BackSpeed:100,
+    backDelay:1000,
+    loop:true
+
+
 });
